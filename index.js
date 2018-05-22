6866d2ff9f09de6182d37e5df4b6b9c2555859da
Uniiicorn <39428293+Uniiicorn@users.noreply.github.com>
2018-05-22 18:30:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -158,4 +158,4 @@ bot.on("message", async message => {
 
     });
 
-bot.login(process.env.BOT_TOKEN); //replace this in case of a download with botconfig.token
+bot.login(process.env.BOT_TOKEN); //replace this in case of a download with botconfig.token 
