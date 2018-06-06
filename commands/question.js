f317b2b50e8c7755c75d2e4b8f2cf9729aaddbdb
Uniiicorn <39428293+Uniiicorn@users.noreply.github.com>
2018-06-06 07:23:05 +0200

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -2,11 +2,94 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    let newArgs = args.slice(0).join(" ");
+
     if(!args[2]) return message.reply("Dude, please ask an full question. Ok? Thank you.");
-    let replies = ["Yes.", "No.", "Maybe.", "Idk.", "Idc.", "Kys.", "Subscribe *Skill.exeTM on YouTubee. NOW.", 
-    "Dude, why the hell are you asking me something like that?", "Hell no.", "Hell yeah!", "Just do it!", 
-    "I like cocks.", "What did you just say?", "Nope.", "Maybe later.", "No. Faggot.", "I told ya, ask an full question.", 
-    "...", "?", "Please, kys. Nub.exe", "sfhsfjkh?", "You're a heartless faggot. Go and kill your self.", "YEEEAAAAHHH!", "Yes, of course"]
+    if(args[1] === "or") {
+        let replies = [args[0], args[2]]
+   
+       
+        let result = Math.floor((Math.random() * replies.length));
+    
+    
+        let question = args.slice(0).join(" ");
+    
+        let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setColor("#ff3300")
+        .addField("Question", question, false)
+        .addField("Answer", replies[result], false)
+    
+        message.channel.send(embed);
+ 
+         return;
+     }
+
+     if(newArgs.includes("how are you")) {
+         let replies = ["Oh, I am fine! What about you?", "Well, I am just a bot but I really appreciate this question! So, I am fine and you?",
+         "I dont know, there are some errors in my code I guess.. I feel kinda sick."]
+    
+        
+         let result = Math.floor((Math.random() * replies.length));
+     
+     
+         let question = args.slice(0).join(" ");
+     
+         let embed = new Discord.RichEmbed()
+         .setAuthor(message.author.tag)
+         .setColor("#ff3300")
+         .addField("Question", question, false)
+         .addField("Answer", replies[result], false)
+     
+         message.channel.send(embed);
+  
+         return;
+  
+    }
+    if(newArgs.includes("are you a good bot")) {
+        let replies = ["Oh, yeah! Of course am I a good bot!", "No.", "Yeah..", "Nope..", "Yes!!"]
+   
+       
+        let result = Math.floor((Math.random() * replies.length));
+    
+    
+        let question = args.slice(0).join(" ");
+    
+        let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setColor("#ff3300")
+        .addField("Question", question, false)
+        .addField("Answer", replies[result], false)
+    
+        message.channel.send(embed);
+ 
+        return; 
+    }
+
+    if(newArgs.includes("kys")) {
+        let replies = ["Why should I kill myself? :(", "Ohh.. okay.. I guess you hate me.. right?", "Okay.. I will hang myself right now.. cya"]
+   
+        let image = "https://i1.wp.com/www.thegamegal.com/wp-content/uploads/2011/06/hangman.jpg?ssl=1";
+
+        let result = Math.floor((Math.random() * replies.length));
+    
+    
+        let question = args.slice(0).join(" ");
+    
+        let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setThumbnail(image)
+        .setColor("#ff3300")
+        .addField("Question", question, false)
+        .addField("Answer", replies[result], false)
+    
+        message.channel.send(embed);
+ 
+        return; 
+    }
+
+    if(newArgs.includes("you love me")) {
+        let replies = ["Yeah.. but.. I can't..", "You should leave me alone now.", "Yes.. I do.", "Oh hell no!", "Dude watch your profile pic, how could I love someone like this?!", "Idk..", "Yeah.. I kinda love you somehow.. I am so sorry.."]
 
     
     let result = Math.floor((Math.random() * replies.length));
@@ -22,8 +105,92 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send(embed);
 
-}
+    return;
+    }
 
+    if(newArgs.includes("you dont love me")) {
+        let replies = ["I do!! But I can't love you..", "No.", "You should leave me alone NOW.", "I am so sorry.. cya man.. " + "❤"]
+
+    
+    let result = Math.floor((Math.random() * replies.length));
+
+
+    let question = args.slice(0).join(" ");
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#ff3300")
+    .addField("Question", question, false)
+    .addField("Answer", replies[result], false)
+
+    message.channel.send(embed);
+
+    return;
+    }
+
+    if(newArgs.includes("you don't love me")) {
+        let replies = ["I do!! But I can't love you..", "No.", "You should leave me alone NOW.", "I am so sorry.. cya man.. " + "❤"]
+
+    
+    let result = Math.floor((Math.random() * replies.length));
+
+
+    let question = args.slice(0).join(" ");
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#ff3300")
+    .addField("Question", question, false)
+    .addField("Answer", replies[result], false)
+
+    message.channel.send(embed);
+
+    return;
+    }
+
+    if(newArgs.includes("some love")) {
+        let replies = ["❤", "I love you man!", "I will always love you!", "No.", "Ohh hell no.", "❤", "❤", "❤"]
+
+    
+    let result = Math.floor((Math.random() * replies.length));
+
+
+    let question = args.slice(0).join(" ");
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#ff3300")
+    .addField("Question", question, false)
+    .addField("Answer", replies[result], false)
+
+    message.channel.send(embed);
+
+    return;
+    }
+
+    let replies = ["Yes.", "No.", "Maybe.", "Idk.", "Idc.", "Dude, why the hell are you asking me something like that?",
+     "Hell no.", "Hell yeah!", "Just do it!", 
+    "What did you just say?", "Nope.", "Maybe later.", 
+    "I told ya, ask an full question.", 
+    "...", "?", "YEEEAAAAHHH!", "Yes, of course"]
+
+    
+    let result = Math.floor((Math.random() * replies.length));
+
+
+    let question = args.slice(0).join(" ");
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#ff3300")
+    .addField("Question", question, false)
+    .addField("Answer", replies[result], false)
+
+    message.channel.send(embed);
+
+    return;
+
+} 
 module.exports.help = {
     name: "?"
 }
